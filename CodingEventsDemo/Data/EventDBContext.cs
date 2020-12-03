fef134d7facf63a53cd92e66db6d181903cc4515
Randy Parker <randallbparker@gmail.com>
2020-12-03 05:55:52 -0600

    before removal of eventdata.cs prepping to use mysql

---
+++ CodingEventsDemo/Data/EventDBContext.cs
@@ -0,0 +1,18 @@
+﻿using CodingEventsDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Data
+{
+    public class EventDBContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        public EventDBContext(DbContextOptions<EventDBContext> options) : base(options)
+        {
+        }
+    }
+}

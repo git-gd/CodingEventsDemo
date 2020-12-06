f98cde639ce5d90596c42c897252797d6e26e706
Randy Parker <randallbparker@gmail.com>
2020-12-06 11:13:26 -0600

    ORM Studio completed

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -24,8 +25,37 @@ namespace CodingEventsDemo.Controllers
         public IActionResult Index()
         {
             ViewBag.title = "All Categories";
-            List<EventCategory> categories = context.Categories.ToList();
+            List<EventCategory> categories = context.EventCategories.ToList();
             return View(categories);
+        }
+
+        // GET: /<controller>/Create/
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            ViewBag.title = "Create A New Category";
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory eventCategory = new EventCategory {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                context.EventCategories.Add(eventCategory);
+                context.SaveChanges();
+
+                return Redirect("/EventCategory");
+            }
+
+            ViewBag.Title = "Create A New Category";
+
+            return View("Create", addEventCategoryViewModel);
         }
     }
 }

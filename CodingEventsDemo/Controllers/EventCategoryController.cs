d73921eb083ee22a9f89aa16012437542181395c
Randy Parker <randallbparker@gmail.com>
2020-12-06 08:58:22 -0600

    EventCategory connected to MySQL, controller setup, view setup - Exercise 19.4 completed

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,32 @@
+﻿using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        //Index() needs to do the following:
+
+        //Responds to GET requests at EventCategory/Index and returns a view called Index.cshtml.
+        //Pass the DbContext category values as a list into the view template as a model.
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<EventCategory> eventCategory = context.EventCategories.ToList();
+
+            return View(eventCategory);
+        }
+    }
+}
--- CodingEventsDemo/Data/EventDbContext.cs
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -8,6 +8,8 @@ namespace CodingEventsDemo.Data
     {
         public DbSet<Event> Events { get; set; }
 
+        public DbSet<EventCategory> EventCategories { get; set; }
+
         public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
         {
         }
